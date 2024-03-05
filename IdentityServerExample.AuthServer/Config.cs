8f43dccdee3fbc90a2b3651b8cd921980388a43f
ozdemiraydemir96 <ozdemiraydemir96@gmail.com>
2024-03-05 15:04:14 +0300

    Add project files.

---
+++ IdentityServerExample.AuthServer/Config.cs
@@ -0,0 +1,63 @@
+﻿using IdentityServer4.Models;
+
+namespace IdentityServerExample.AuthServer;
+
+public static class Config
+{
+    public static IEnumerable<ApiResource> GetApiResources()
+    {
+        return new List<ApiResource>()
+        {
+            new ApiResource("resource_api1")
+            {
+                Scopes={ "api1.read", "api1.write", "api1.update" },
+                ApiSecrets=new[]{new Secret("secretapi1".Sha256()
+                )}
+            },
+            new ApiResource("resource_api2")
+            {
+                Scopes={ "api2.read", "api2.write", "api2.update" },
+                 ApiSecrets=new[]{new Secret("secretapi2".Sha256()
+                )}
+            }
+        };
+    }
+    public static IEnumerable<ApiScope> GetApiScopes()
+    {
+        return new List<ApiScope>()
+        {
+            new ApiScope("api1.read","API 1 için okuma izni"),
+            new ApiScope("api1.write","API 1 için yazma izni"),
+            new ApiScope("api1.update","API 1 için güncelleme izni"),
+
+               new ApiScope("api2.read","API 2 için okuma izni"),
+            new ApiScope("api2.write","API 2 için yazma izni"),
+            new ApiScope("api2.update","API 2 için güncelleme izni"),
+        };
+    }
+
+
+    public static IEnumerable<Client> GetClient()
+    {
+        return new List<Client>()
+        {
+            new Client() {ClientId="client1",
+                ClientSecrets=new[]{new Secret("secret".Sha256())},
+            ClientName="Client 1 uygulaması",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+            AllowedScopes= { "api1.read" , "api2.write", "api2.update" }
+             },
+
+              new Client() {ClientId="client2",
+                ClientSecrets=new[]{new Secret("secret".Sha256())},
+            ClientName="Client 2 uygulaması",
+                AllowedGrantTypes=GrantTypes.ClientCredentials,
+            AllowedScopes= { "api1.read" , "api2.write", "api2.update" }
+             }
+
+
+
+
+          };
+    }
+}
---
+++ IdentityServerExample.Client1/Controllers/ProductController.cs
@@ -0,0 +1,41 @@
+﻿using IdentityModel.Client;
+using IdentityServerExample.Client1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace IdentityServerExample.Client1.Controllers;
+
+public class ProductController : Controller
+{
+    private readonly IConfiguration configuration;
+    private  HttpClient client;
+
+    public ProductController(IConfiguration configuration)
+    {
+        this.configuration = configuration;
+        client = new HttpClient();
+    }
+    public async Task< IActionResult> Index()
+    {
+        DiscoveryDocumentResponse discovery = await client.GetDiscoveryDocumentAsync("https://localhost:7009");
+        ClientCredentialsTokenRequest clientCredentialsTokenRequest= new ClientCredentialsTokenRequest() { ClientId = configuration["Client:ClientId"], ClientSecret = configuration["Client:ClientSecret"],Address=discovery.TokenEndpoint };
+        TokenResponse token= await client.RequestClientCredentialsTokenAsync(clientCredentialsTokenRequest);
+        client.SetBearerToken(token.AccessToken);
+        HttpResponseMessage response = await client.GetAsync("https://localhost:7010/api/products/getproducts");
+        if (response.IsSuccessStatusCode)
+        {
+            var content= await response.Content.ReadAsStringAsync();
+            List<Product> products = new();
+             products = JsonConvert.DeserializeObject<List<Product>>(content);
+
+            return View(products);
+        }
+        else
+        {
+//loglama
+        }
+
+        return View();
+    }
+}
